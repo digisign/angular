93b2a71638f8860d41c440f82162ba1ecae8dfb2
Sudhanshu Bhardwaj <sudhanshusbsingh@gmail.com>
2018-09-13 15:42:21 +0530

    ui changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular';
+  title = 'Digital Credentials';
+  static API_URL="http://localhost:8080";
 }
